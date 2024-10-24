792db8de56ad35dc6d8dd7b6916d68f49c5fcaeb
Hamza Kader <hakader@c4r9p3.1337.ma>
2024-10-24 12:32:31 +0100

    p

---
+++ ft_strnstr.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hakader <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/24 09:53:39 by hakader           #+#    #+#             */
+/*   Updated: 2024/10/24 12:21:39 by hakader          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <string.h>
+
+char	*strnstr(const char *big, const char *little, size_t len)
+{
+	size_t	i;
+	size_t	j;
+	size_t	len_big;
+	size_t	len_lit;
+
+	i = 0;
+	j = 0;
+	len_big = strlen(big);
+	len_lit = strlen(little);
+	if (len_lit == 0 || len_lit == NULL
+		|| len_lit > len_big)
+		return (NULL);
+	while (big[i])
+	{
+		if (big[i] == little[j])
+		{
+			j++;
+		}
+		i++;
+	}
+	if (little[j] == '\0')
+	{
+		return (little);
+	}
+	else
+		return (NULL);
+}
