b43194c1d90f24191f409b70ef9db4461b32be27
Hamza Kader <hakader@e1r9p2.1337.ma>
2024-10-25 11:57:12 +0100

    p

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,29 +6,39 @@
 /*   By: hakader <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/23 20:36:35 by hakader           #+#    #+#             */
-/*   Updated: 2024/10/24 09:49:03 by hakader          ###   ########.fr       */
+/*   Updated: 2024/10/25 11:56:55 by hakader          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 
-size_t	strlcat(char *restrict dst, const char *restrict src, size_t dstsize)
+size_t	ft_strlen(const char *s)
 {
-	size_t	i;
-	size_t	j;
+	size_t	x;
 
+	x = 0;
+	while (s[x])
+		x++;
+	return (x);
+}
+
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
+{
+	size_t	dst_len;
+	size_t	src_len;
+	size_t	i;
+
+	dst_len = ft_strlen(dst);
+	src_len = ft_strlen(src);
 	i = 0;
-	j = 0;
-	while (dst[i])
+	if (dstsize > dst_len + src_len + 1)
 	{
-		i++;
+		while (src[i])
+		{
+			dst[dst_len] = src[i];
+			i++;
+			dst_len++;
+		}
+		return (dst_len);
 	}
-	while (dstsize > 0 && src[j])
-	{
-		dst[i] = src[j];
-		i++;
-		j++;
-	}
-	dst[j] = '\0';
-	return (dst);
 }
--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,25 +6,28 @@
 /*   By: hakader <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/23 20:17:38 by hakader           #+#    #+#             */
-/*   Updated: 2024/10/24 09:49:28 by hakader          ###   ########.fr       */
+/*   Updated: 2024/10/25 10:49:12 by hakader          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 
-size_t	strlcpy(char *restrict dst, const char *restrict src, size_t dstsize)
+size_t	ft_strlcpy(char *restrict dst, const char *restrict src, size_t dstsize)
 {
 	size_t	i;
 
 	i = 0;
-	while (dstsize > 0)
+	if (dstsize > 0)
 	{
-		if (src[i])
+		while (src[i] && i < dstsize - 1)
 		{
 			dst[i] = src[i];
 			i++;
 		}
-		dstsize--;
 	}
-	return (dst);
+	dst[i] = '\0';
+	i = 0;
+	while (src[i])
+		i++;
+	return (i);
 }
