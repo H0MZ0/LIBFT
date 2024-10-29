a9d47ea9d3bf4a7d6b580d8312e05193012c752c
Hamzared2500 <hamzared2500@gmail.com>
2024-10-29 17:57:01 +0100

    p

--- additional_functions/ft_strtrim.c
+++ additional_functions/ft_strtrim.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strtrim.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hakader <hakader@student.42.fr>            +#+  +:+       +#+        */
+/*   By: hakader <hakader@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/27 12:41:38 by hakader           #+#    #+#             */
-/*   Updated: 2024/10/28 12:37:15 by hakader          ###   ########.fr       */
+/*   Updated: 2024/10/29 12:10:29 by hakader          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,10 @@ char	*ft_strtrim(char const *s1, char const *set)
 	char	*aloc;
 	size_t	i;
 	size_t	j;
+	size_t	k;
 	size_t	s1_len;
+	size_t	aloc_len;
+	unsigned char	*aloc;
 
 	i = 0;
 	s1_len = ft_strlen(s1);
@@ -28,9 +31,31 @@ char	*ft_strtrim(char const *s1, char const *set)
 		return (NULL);
 	if (set[0] == NULL)
 		return (s1);
-	while (s1[i] && s1[i] != set)
+	if (s1[i])
 	{
-		if (ft_strchr(set, s1[i]) == NULL)
-			
+		while (ft_strchr(set, s1[i]) == NULL)
+			i++;
+		while (ft_strchr(set, s1[i] == s1[i]))
+			i++;
 	}
+	if (s1_len > 0)
+	{
+		while (ft_strchr(set, s1[s1_len]) == NULL)
+			s1_len--;
+		while (ft_strchr(set, s1[s1_len]) == s1[s1_len])
+			s1_len--;
+	}
+	if (i >= s1_len)
+		return (s1);
+	aloc_len = s1_len - i;
+	aloc = malloc(sizeof(aloc_len));
+	k = 0;
+	while (i <= s1_len)
+	{
+		aloc[k] = s1[i];
+		i++;
+		k++;
+	}
+	aloc[k] = '\0';
+	return(aloc);
 }
