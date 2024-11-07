6a051cc0b5a84347c2c0b6f7a3b063fafde095f9
Hamza Kader <hakader@e1r5p10.1337.ma>
2024-11-07 21:23:11 +0100

    p

--- ft_putendl_fd.c
+++ ft_putendl_fd.c
@@ -6,11 +6,12 @@
 /*   By: hakader <hakader@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/31 16:08:16 by hakader           #+#    #+#             */
-/*   Updated: 2024/11/05 22:59:31 by hakader          ###   ########.fr       */
+/*   Updated: 2024/11/07 21:19:20 by hakader          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <fcntl.h>
 
 void	ft_putendl_fd(char *s, int fd)
 {
@@ -18,4 +19,10 @@ void	ft_putendl_fd(char *s, int fd)
 		return ;
 	ft_putstr_fd(s, fd);
 	write(fd, "\n", 1);
+}
+
+int main()
+{
+	int fd = open("test.txt", O_WRONLY | O_RDONLY | O_CREAT , 0777);
+	ft_putendl_fd("hello",fd);
 }
