73aae741b945e9b08837090cea06519eab93a6de
H0MZ0 <hamzagamer2500@gmail.com>
2024-11-09 21:31:01 +0100

    v2

--- ft_lstnew_bounus.c
+++ ft_lstnew_bounus.c
@@ -1,34 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*   ft_lstnew_bounus.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hakader <hakader@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/10/26 17:36:54 by hakader           #+#    #+#             */
-/*   Updated: 2024/11/09 16:33:27 by hakader          ###   ########.fr       */
+/*   Created: 2024/11/09 18:37:46 by hakader           #+#    #+#             */
+/*   Updated: 2024/11/09 21:30:50 by hakader          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_substr(char const *s, unsigned int start, size_t len)
+t_list *ft_lstnew(void *content)
 {
-	char	*str;
-	size_t	s_len;
-	size_t	end;
-
-	s_len = ft_strlen(s);
-	if (!s)
-		return (NULL);
-	end = 0;
-	if (start < s_len)
-		end = s_len - start;
-	if (end > len)
-		end = len;
-	str = (char *)malloc(sizeof(char) * (end + 1));
-	if (!str)
-		return (NULL);
-	ft_strlcpy(str, s + start, end + 1);
-	return (str);
-}
+    t_list *new;
+} t_list;
--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: hakader <hakader@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/24 12:23:38 by hakader           #+#    #+#             */
-/*   Updated: 2024/11/06 12:59:23 by hakader          ###   ########.fr       */
+/*   Updated: 2024/11/09 15:36:52 by hakader          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,17 +21,10 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 	i = 0;
 	s = (unsigned char *)src;
 	d = (unsigned char *)dest;
-	if (!n)
-		return (d);
-	if (!s || !d)
-		return (NULL);
-	if (s[i] && d[i])
+	while (i < n)
 	{
-		while (i < n)
-		{
-			d[i] = s[i];
-			i++;
-		}
+		d[i] = s[i];
+		i++;
 	}
 	return (d);
 }
